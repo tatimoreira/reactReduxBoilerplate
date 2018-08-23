a552ec170d9522594f2e56e939800a932d9196cb
Tatiana Moreira <tatimb14@gmail.com>
2018-08-23 14:29:20 -0600

    Configuring store

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import 'babel-polyfill';
 import React from 'react';
 import {render} from 'react-dom';
+import configureStore from './store/configureStore';
+import {Provider} from 'react-redux';
 import {Router, browserHistory} from 'react-router';
 import routes from './routes';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
@@ -8,6 +10,8 @@ import '../node_modules/toastr/build/toastr.min.css';
 
 
 render(
-  <Router history={browserHistory} routes={routes}/>,
-  document.getElementById('app')
+	<Provider store={store} >
+		<Router history={browserHistory} routes={routes}/>
+	</Provider>,
+	document.getElementById('app')
 );
---
+++ src/reducers/index.js
@@ -0,0 +1,14 @@
+import {combineReducers} from 'redux';
+
+//Pain attetion how to name the alias of the reducers 
+//this is what going to be used
+//along the rootreducer to reference the reducer and through the app
+import courses from './courseReducer'
+
+const rootReducer = combineReducers ({
+	//use better the shorthand ES6 form than courses: courses
+	courses
+
+});
+
+export default rootreducer; 
