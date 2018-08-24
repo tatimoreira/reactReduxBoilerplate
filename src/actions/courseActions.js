223b32ec1ac4e13bc9660a8355eea39be8d12d35
Tatiana Moreira <tatimb14@gmail.com>
2018-08-24 13:35:05 -0600

    Added a file with constant types of actions

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,6 +1,6 @@
-//crear la action type obliga
+import * as types from './actionTypes';
 
 export  function createCourse (course) {
 	//ES6 shorthand property in contrast in ES5 would be course:course
-	return {type: "CREATE_COURSE" , course};
+	return {type: types.CREATE_COURSE , course};
 }
--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -1,11 +1,12 @@
 // A reducer is a function that receives and state and action and return a new state
 
+import * as types from '../actions/actionTypes';
 
 //in ES6 ones can define a default in the input parameters
 //the state=[] represents the list of course
 export default function courseReducer (state=[], action ){
 	switch (action.type) {
-		case 'CREATE_COURSE' :
+		case types.CREATE_COURSE :
 
 		//returns a brand-new state that contains the course passed in the action 
 		// The next code is wrong because the inmutabllty of state
