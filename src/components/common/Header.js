ef213feed02c8262a7980b110c2b8122f1f3586d
Tatiana Moreira <tatimb14@gmail.com>
2018-08-28 17:32:46 -0600

    Added a preloader to indicate ajax status

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,6 +1,6 @@
 import React, {PropTypes} from 'react';
 import {Link, IndexLink} from 'react-router';
-
+import LoadingDots from './LoadingDots';
 const Header = () => {
   return (
     <nav>
@@ -9,6 +9,7 @@ const Header = () => {
       <Link to="/courses" activeClassName="active">Courses</Link>
       {" | "}
       <Link to="/about" activeClassName="active">About</Link>
+      <LoadingDots interval={100} dots={20} />
     </nav>
   );
 };
